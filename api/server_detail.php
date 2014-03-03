ac490c30815b1c4af006a1da661da3da465c25a1
Andre Courchesne <andre.courchesne@intello.com>
2014-03-03 15:17:58 -0500

    - Use id parameter

--- api/server_detail.php
+++ api/server_detail.php
@@ -3,7 +3,7 @@ $username="monitor";
 $password="etc1brew";
 $database="monitor";
 
-//$first=$_POST['first'];
+$id=$_GET['id'];
 //$last=$_POST['last'];
 //$phone=$_POST['phone'];
 //$mobile=$_POST['mobile'];
@@ -14,7 +14,7 @@ $database="monitor";
 mysql_connect('localhost',$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 
-$query = "select config_data from monitor_servers limit 1";
+$query = "select config_data from monitor_servers where server_id='".$id."'";
 $result=mysql_query($query);
 $num=mysql_numrows($result);
 mysql_close();
